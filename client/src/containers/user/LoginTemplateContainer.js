db90cb69f7d4a3178c1ede6bc524154e956e8d07
micahang0127 <psa5013@hanmail.net>
2020-02-21 12:45:41 +0900

    react reduce test

--- client/src/containers/user/LoginTemplateContainer.js
+++ client/src/containers/user/LoginTemplateContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LoginTemplate from '../../components/user/LoginTemplate';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import {loginSuccess, loginFail}  from '../../actions/user.action'
 // import { bidnActionCreators } from 'redux';
 // import * as baseAction from './store/modules/base';
 
@@ -24,6 +25,7 @@ class LoginTemplateContainer extends Component {
 
     handleSubmit(e){
         e.preventDefualt();
+        this.props.loginSuccess();
     }
 
     handleChange(e){
@@ -38,14 +40,15 @@ class LoginTemplateContainer extends Component {
 
     render() {
 
-        const { username, password, handleSubmit, handleChange, handleKeyPress} = this;
-        // action 데이터는 const{ } = this.props 로 다시 정의하기
+        const { username, password,  handleChange, handleKeyPress} = this;
+        // action 데이터
+        // cosnt { handleSubmit } = this.props.isLoggedIn;
 
         return (
             <LoginTemplate 
                 username={this.username}
                 password={this.password}
-                handleSumit={this.handleSubmit}
+                handleSubmit={this.handleSubmit}
                 handleChange={this.handleChange}
                 handleKeyPress={this.handleKeyPress}
             /> 
@@ -53,9 +56,24 @@ class LoginTemplateContainer extends Component {
     }
 }
 
+// getState() => props형태로 사용
+// const mapStateToProps = (state) => {
+//     return {
+//         // isLoggedIn : state.isLoggedIn,
+//         user : state.user
+//     };
+// }
+// = (state) => ({  isLoggenIn: ~~ });
 
-// connect 
-// (state) =>
-// (dispatch) =>
+// 액션상태를 바꿀 때 사용, 액션 객체는 함수를 반환!
+const dispatchStateToProps = (dispatch) => {
+    return {
+        loginSuccess : () => dispatch(loginSuccess()),
+        loginFail : () => dispatch(loginFail())
+    };
+}
+
+LoginTemplateContainer = connect(undefined, dispatchStateToProps)(LoginTemplateContainer);
+// connect는 react-redux의 내장 aip로, component를 redux Store에 '연결'해준다.
 
 export default LoginTemplateContainer;

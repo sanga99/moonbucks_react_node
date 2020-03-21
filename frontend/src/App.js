883c67b955426a706203fa9cb088dd8ba2179afc
micahang0127 <psa5013@hanmail.net>
2020-03-22 05:37:01 +0900

    eject 실수로 파일 재생성 -> github testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { NotFoundPage, LoginPage, RegisterPage, AdminHomePage, OwnerHomePage} from './pages';
 import './styles/base.scss';
+// yarn eject는 함부로 사용하지 말것. -> (주의)한번 사용하면 되돌릴 수 없다!!
+// 현재상황 :
+// yarn eject -> git discard (eject를 되돌리려 eject로 변화된 파일 되돌림) -> 문제발생!! client서버가 작동하지 않음
+// 해결 : 
+// -> github에서 clone으로 다시 파일 생성 -> git commit까지 잘 되는지 test중
 
 class App extends Component {
 

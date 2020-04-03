bc3328f77fc6d7a2c941d50d24ce8e4a56a62300
micahang0127 <psa5013@hanmail.net>
2020-04-04 02:15:01 +0900

    mp4 동영상 문구

--- frontend/src/components/map/MapVidio.js
+++ frontend/src/components/map/MapVidio.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import myVideo from './moonbucks_map.mp4';
+import './Map.css';
 
 
 class MapVidio extends Component {
     render() {
         return (
-        <div className='map_wrap vidio'>
+        <div className='vidio'>
+            <div className='text'>배포환경(Heroku)에서 kako Map마커에 인식오류가 있어 잠시 영상으로 대체합니다.</div>
             <ReactPlayer
                 className='react-player fixed-bottom'
                 url= {myVideo}
